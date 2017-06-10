20989bc18b2b7a177f8a15e476db32c337355bed
cwxyz <jie844067636@gmail.com>
2017-06-10 14:04:55 +0800

    modify css

--- index.js
+++ index.js
@@ -2,19 +2,36 @@
  * Created by 84406 on 2017/6/10.
  */
 
-const TitleFontSize = 125;
-const BackgroundColor = 0x0061bb;
-
-const GridWidth = 60;
-const GridDelta = 10;
-const GridStartX = 60;
-const GridStartY = 250;
-const GridColor = 0xFF700B;
+const BackgroundColor = 0xFCE38A;
 
 var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: BackgroundColor});
 document.body.appendChild(app.view);
 
-var basicText = new PIXI.Text('2048', {fontSize: TitleFontSize});
+const GridWidth = 60;
+const GridDelta = 10;
+const GridStartX = (app.renderer.width - ((GridDelta + GridWidth) * 4 - GridDelta)) / 2;
+const GridStartY = app.renderer.height / 8 * 3;
+const GridColor = 0x95E1D3;
+
+var titleStyle = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 125,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fill: ['#EAFFD0'], // gradient
+    stroke: '#222831',
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: '#393E46',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 440
+});
+
+var basicText = new PIXI.Text('2048', titleStyle);
+
 basicText.anchor.set(0.5);
 basicText.x = app.renderer.width / 2;
 basicText.y = app.renderer.height / 5;
@@ -28,7 +45,6 @@ for (var i = 0; i < 4; i++) {
         app.stage.addChild(graphics);
     }
 }
-
 
 var grid = [];
 
